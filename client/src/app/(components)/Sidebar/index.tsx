08b7372ad434c7590c3ba69fe4917424b21e1a19
Humphrey Ikhalea <134397026+humphreydev5@users.noreply.github.com>
2024-11-29 13:05:39 +0100

    Update index.tsx

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -93,7 +93,7 @@ const Sidebar = () => {
             isSidebarCollapsed ? "hidden" : "block"
           } font-extrabold text-2xl`}
         >
-          EDSTOCK
+          BIZSTOCK
         </h1>
 
         <button
