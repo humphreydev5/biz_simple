eb07e6d3510d8b5c76ccab4194a008e2fdc1df5a
Humphrey Ikhalea <134397026+humphreydev5@users.noreply.github.com>
2024-11-29 18:42:20 +0100

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "inventory-m-s3-bucket.s3.eu-north-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
